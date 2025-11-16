6d9cb0b48caefd719077541b2e0def7e518afa51
linus lind <linuslind3@gmail.com>
2025-11-16 16:11:20 +0100

    Camera layers and mouse lock
    Quick implementation of a weapon layer to prevent weapon clipping and also added in mouse lock so its not visible

---
+++ Assets/Scripts/UI/TempMouse.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class TempMouse : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
